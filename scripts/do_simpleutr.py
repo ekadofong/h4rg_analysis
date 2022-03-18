68b40f26d740fe9f41fea744af8b81627862bd45
Erin Kado-Fong <kadofong@princeton.edu>
2022-03-18 10:20:23 -0400

    add chunking

--- scripts/do_simpleutr.py
+++ scripts/do_simpleutr.py
@@ -44,19 +44,33 @@ def memusage ():
     process = psutil.Process(os.getpid())
     print(process.memory_info().rss/1e9) 
 
-def singleton ( ramp, frametype, tag=None, r0=0, r1=50, nbins=4, savedir=None, verbose=True ):
-    if savedir is None:
-        savedir = _DEFAULT_OUTPUTDIR
-    if frametype == 'irp':
-        frame_label = 'IRP'
-        tot_ref = io.irpStack_memmap (ramp, r0=r0, r1=r1)
-    elif frametype == 'data':
-        frame_label = 'data'
-        tot_ref = ramp.dataStack (r0=r0, r1=r1)
+def singleton ( ramp, frame_label, tag=None, r0=0, r1=-1, nbins=10, 
+               savedir=None, verbose=True, total_start=None,
+               clobber=False ):
+    if total_start is None:
+        total_start = datetime.datetime.now ()
+    individ_start = datetime.datetime.now ()
+    
     if tag is None:
         tag = f'RND{np.random.randint(0,1000)}'
+    prefix = f'{tag}_{frame_label}_S{r0}-E{r1}'
+    if not clobber and (os.path.exists(f'{savedir}{prefix}_var.npy')):
+        print(f'{prefix} already run.')
+        return 0
     
     
+    if savedir is None:
+        savedir = _DEFAULT_OUTPUTDIR
+    if frame_label == 'IRP':
+        #frame_label = 'IRP'
+        tot_ref = io.irpStack_memmap (ramp, r0=r0, r1=r1)
+    elif frame_label == 'data':
+        #frame_label = 'data'
+        tot_ref = io.dataStack_memmap (ramp, r0=r0, r1=r1)
+    elif frame_label == 'corr':
+        #frame_label = 'corr'
+        tot_ref = io.corrStack_memmap (ramp, r0=r0, r1=r1 )
+
     if verbose:
         print (f'starting {tag}')
         sys.stdout.flush()
@@ -67,49 +81,47 @@ def singleton ( ramp, frametype, tag=None, r0=0, r1=50, nbins=4, savedir=None, v
     
     var_arr = np.zeros([ramp.ncols, ramp.nrows] )
     param_arr = np.zeros([2, ramp.ncols, ramp.nrows])
-    if not os.path.exists (f"{savedir}{tag}"):
-        os.mkdir(f"{savedir}{tag}")
-    prefix = f'{tag}_{frame_label}_S{r0}-E{r1}'    
-    
-    j=0
-    total_start = datetime.datetime.now ()
+
+    j=0    
     for cslice_y in slices:
         for cslice_x in slices:
-            individ_start = datetime.datetime.now ()
+            
             ref = tot_ref[:,cslice_y, cslice_x] #io.irp_sliced ( ramp, slicey=cslice_y, slicex=cslice_x )
-            logn ( f'{frame_label} loaded', individ_start, total_start )
+            #logn ( f'{frame_label} loaded', individ_start, total_start )
+
+            var, params = utrstats ( ref )
+            #logn ( 'Stats computed', individ_start, total_start )
             
+            var_arr[cslice_y, cslice_x] = var
+            param_arr[:, cslice_y, cslice_x] = params
+            #logn(f'''Completed :: {cslice_y}, {cslice_x}''', individ_start, total_start )
             
-            #\\ var, params = utrstats ( ref )
-            #\\ logn ( 'Stats computed', individ_start, total_start )
-            #\\ 
-            #\\ var_arr[cslice_y, cslice_x] = var
-            #\\ param_arr[:, cslice_y, cslice_x] = params
-            #\\ logn(f'''Completed :: {cslice_y}
-            #\\  {cslice_x}''', individ_start, total_start )
-            #\\ 
-            #\\ np.save ( f'{scratchdir}/{prefix}_var{j}.npy', var )
-            #\\ np.save ( f'{scratchdir}/{prefix}_utrparams{j}.npy', params )
-            #\\ del ref, var, params
+            np.save ( f'{scratchdir}/{prefix}_var{j}.npy', var )
+            np.save ( f'{scratchdir}/{prefix}_utrparams{j}.npy', params )
+            del ref, var, params
             j+=1
     
     np.save ( f'{savedir}{prefix}_var.npy', var_arr )
     np.save( f'{savedir}{prefix}_utrparam.npy', param_arr )
+    logn (f'Completed {prefix}',  individ_start, total_start)
 
 if 'SLURM_ARRAY_TASK_ID' in os.environ.keys():
     idx = int(os.environ['SLURM_ARRAY_TASK_ID'])
-    
-    if idx < (len(bestbaselines)//2-1):
-        worklist = bestbaselines[idx*2:idx*2+2]
+    chunk = 2
+    if idx < (len(bestbaselines)//chunk-1):
+        worklist = bestbaselines[idx*chunk:idx*chunk+chunk]
     else:
-        worklist = bestbaselines[idx*2:]
+        worklist = bestbaselines[idx*chunk:]
+        
+    total_start = datetime.datetime.now ()
     for task in worklist:
         pt = io.jhu2tiger ( task )
         ramp = hxramp.HxRamp( pt )     
-        tag = os.path.basename(pt).strip('.fits')   
-        if ramp.interleaveOffset > 0:
-            singleton ( ramp, 'irp', tag=tag )
-        #singleton ( ramp, 'data', tag=tag)
-        
-    
+        tag = os.path.basename(pt).strip('.fits')           
+        kwargs = {'tag':tag,'total_start':total_start}
+        #if ramp.interleaveOffset > 0:
+        #    singleton ( ramp, 'irp', **kwargs)
+        singleton ( ramp, 'data',    **kwargs)
+        singleton ( ramp, 'corr',    **kwargs)
+
         
