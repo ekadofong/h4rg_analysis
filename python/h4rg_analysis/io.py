5da56d557824375629f6e184582584fd74246c9c
Erin Kado-Fong <kadofong@princeton.edu>
2022-04-06 13:56:02 -0400

    add irp memmapping

--- python/h4rg_analysis/io.py
+++ python/h4rg_analysis/io.py
@@ -29,45 +29,69 @@ def irp_sliced ( ramp, r0=0, r1=-1, slicey=None, slicex=None ):
         cimage[r_i] = irpImage[slicey,slicex]    
     return cimage
 
-def irpStack_memmap ( ramp, r0=0, r1=-1, filename=None ):
+def irpStack_memmap ( ramp, r0=0, r1=-1, filename=None, slicey=None, slicex=None ):
     if filename is None:        
         filename = tempfile.NamedTemporaryFile()
+    if slicey is None:
+        shape_y = ramp.nrows
+    else:
+        shape_y = min(ramp.nrows, slicey.stop) - slicey.start
+    if slicex is None:
+        shape_x = ramp.ncols
+    else:
+        shape_x = min(ramp.ncols, slicex.stop) - slicex.start               
         
     r0 = ramp._readIdxToAbsoluteIdx ( r0 )
     r1 = ramp._readIdxToAbsoluteIdx ( r1 )    
-    fp = np.memmap ( filename, dtype='u2', mode='w+', shape=(r1-r0+1, ramp.nrows, ramp.ncols ))
+    fp = np.memmap ( filename, dtype='u2', mode='w+', shape=(r1-r0+1, shape_y, shape_x )) 
     for r_i in np.arange(r0, r1+1):
         n = ramp._readIdxToFITSIdx ( r_i )
         extname = f'REF_{n}'
         irpImage = ramp.fits[extname].read ()
         irpImage = hxramp.constructFullIrp(irpImage, ramp.nchan, refPix=ramp.interleaveOffset)
-        fp[r_i] = irpImage
+        fp[r_i] = irpImage[slicey,slicex]
     fp.flush ()
     return fp, filename
 
-def dataStack_memmap ( ramp, r0=0, r1=-1, filename=None ):
+def dataStack_memmap ( ramp, r0=0, r1=-1, filename=None, slicey=None, slicex=None ):    
     if filename is None:
         filename = tempfile.NamedTemporaryFile()
+    if slicey is None:
+        shape_y = ramp.nrows
+    else:
+        shape_y = min(ramp.nrows, slicey.stop) - slicey.start
+    if slicex is None:
+        shape_x = ramp.ncols
+    else:
+        shape_x = min(ramp.ncols, slicex.stop) - slicex.start          
         
     r0 = ramp._readIdxToAbsoluteIdx ( r0 )
     r1 = ramp._readIdxToAbsoluteIdx ( r1 )
-    fp = np.memmap ( filename, dtype='u2', mode='w+', shape=(r1-r0+1, ramp.nrows, ramp.ncols ))    
+    fp = np.memmap ( filename, dtype='u2', mode='w+', shape=(r1-r0+1, shape_y, shape_x )) 
     for r_i in np.arange(r0, r1+1):
         n = ramp._readIdxToFITSIdx ( r_i )
         extname = f'IMAGE_{n}'
         dataImage = ramp.fits[extname].read ()
-        fp[r_i] = dataImage
+        fp[r_i] = dataImage[slicey,slicex]
     fp.flush ()
     return fp, filename
 
 
-def corrStack_memmap ( ramp, r0=0, r1=-1, filename=None):
+def corrStack_memmap ( ramp, r0=0, r1=-1, slicey=None, slicex=None, filename=None):
     if filename is None:
         filename = tempfile.NamedTemporaryFile()
+    if slicey is None:
+        shape_y = ramp.nrows
+    else:
+        shape_y = min(ramp.nrows, slicey.stop) - slicey.start
+    if slicex is None:
+        shape_x = ramp.ncols
+    else:
+        shape_x = min(ramp.ncols, slicex.stop) - slicex.start        
         
     r0 = ramp._readIdxToAbsoluteIdx ( r0 )
     r1 = ramp._readIdxToAbsoluteIdx ( r1 )
-    fp = np.memmap ( filename, dtype='u2', mode='w+', shape=(r1-r0+1, ramp.nrows, ramp.ncols ))        
+    fp = np.memmap ( filename, dtype='u2', mode='w+', shape=(r1-r0+1, shape_y, shape_x ))        
     
     for r_i in np.arange(r0, r1+1):
         n = ramp._readIdxToFITSIdx ( r_i )
@@ -78,9 +102,9 @@ def corrStack_memmap ( ramp, r0=0, r1=-1, filename=None):
             ref_extname = f'REF_{n}'
             irpImage = ramp.fits[ref_extname].read ()
             irpImage = hxramp.constructFullIrp(irpImage, ramp.nchan, refPix=ramp.interleaveOffset)
-            corrImage = dataImage - irpImage
+            corrImage = (dataImage - irpImage)[slicey,slicex]
         except: # \\ no IRP frame
-            corrImage = dataImage
+            corrImage = dataImage[slicey,slicex]
         
         fp[r_i] = corrImage
     fp.flush ()
