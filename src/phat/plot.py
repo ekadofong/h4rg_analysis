93066a8ae5d9edc9856b3a084c50eb659d1062dc
Erin Kado-Fong <kadofong@princeton.edu>
2022-05-11 10:10:14 -0400

    plotting utilities

---
+++ src/phat/plot.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
+
+def adjust_value ( color, factors=(1.,1.,0.8) ):
+    if isinstance(color, str):
+        color = colors.to_rgb ( color )
+    chsv = colors.rgb_to_hsv ( color )
+    chsv = chsv * np.array(factors)
+    crgb = colors.hsv_to_rgb ( chsv )  
+    return crgb  
+
+def scaled_imshow ( im, ax=None, beta=0.01, colorbar=True, interpolation=None, label=None ):
+    if ax is None:
+        ax = plt.subplot(111)
+    vmin, vmax = np.nanquantile ( im, [beta, 1. - beta] )
+    if interpolation is not None:
+        imout = ax.imshow ( im, vmin=vmin, vmax=vmax, interpolation=interpolation )
+    else:
+        imout = ax.imshow ( im, vmin=vmin, vmax=vmax)
+    if colorbar:
+        plt.colorbar ( imout, ax=ax, label=label )
+    return ax
+
+def scale_ylim(y, ax, eta=0.005):
+    lims = np.quantile(y,[eta,1.-eta])
+    ax.set_ylim(lims)
